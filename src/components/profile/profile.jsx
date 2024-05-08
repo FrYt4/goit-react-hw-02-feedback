348e9bd69944fa19ccb18fc1e5515d6f9d89e73d
FrYt4 <adrian.fryczkie@yahoo.com>
2024-05-08 21:11:32 +0200

    first

--- src/components/profile/profile.jsx
+++ src/components/profile/profile.jsx
@@ -13,7 +13,7 @@ export default function UserProfile({
       <div className={css.description}>
         <img src={avatar} alt="User avatar" className={css.avatar} />
         <p className={css.name}>{username}</p>
-        <p className={css.tag}>{tag}</p>
+        <p className={css.tag}>@{tag}</p>
         <p className={css.location}>{location}</p>
       </div>
 
