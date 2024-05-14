15044ea40aea9b5ae90e980e96040ef795206d2f
FrYt4 <adrian.fryczkie@yahoo.com>
2024-05-14 17:40:11 +0200

    a

--- src/App.jsx
+++ src/App.jsx
@@ -1,21 +1,16 @@
-import UserProfile from "components/profile/profile";
-import user from "./assets/user.json";
-import Statistics from "components/statistics/statistics";
-import data from "./assets/data.json";
-import FriendList from "components/friendslist/friendlist";
-import friends from "./assets/friends.json"; 
-import TransactionHistory from "components/transationhistory/transationhistory";
-import transactions from "./assets/transactions.json";
-
 export const App = () => {
   return (
     <div
+      style={{
+        height: '100vh',
+        display: 'flex',
+        justifyContent: 'center',
+        alignItems: 'center',
+        fontSize: 40,
+        color: '#010101'
+      }}
     >
-      <UserProfile {...user} />
-      <Statistics title="Upload stats" stats={data} />
-      <FriendList friends={friends} />
-      <TransactionHistory items={transactions}/>
-
+      React homework templateaaa
     </div>
   );
 };
